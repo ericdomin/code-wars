5a8b16e6c8c69c3bedde50a0ed1255065efe230a
Eric Domin <ericmdomin@gmail.com>
2022-12-22 17:09:33 -0500

    new codewars

---
+++ sevenKyu/remove-url-anchor.js
@@ -0,0 +1,8 @@
+function removeUrlAnchor(url){
+    // TODO: complete
+    ///find index of #
+    let ind = url.indexOf('#')
+    //check if index>-1
+    //slice the part of string before index of #
+    return ind>-1 ? url.slice(0,ind) : url
+  }
