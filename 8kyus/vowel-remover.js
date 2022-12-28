71c195f522365bdda764a1a3c13163ac833b8636
Eric Domin <ericmdomin@gmail.com>
2022-12-28 13:42:25 -0500

    codewars

---
+++ 8kyus/vowel-remover.js
@@ -0,0 +1,17 @@
+/*Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+Examples
+"hello"     -->  "hll"
+"codewars"  -->  "cdwrs"
+"goodbye"   -->  "gdby"
+"HELLO"     -->  "HELLO" 
+don't worry about uppercase vowels
+y is not considered a vowel for this kata
+*/
+function shortcut (string) {
+    let arr=[];
+   for(let i=0;i<string.length;i++){
+    if(string[i] !='a' && string[i]!= 'e' && string[i]!= 'i' && string[i]!='o' && string[i]!='u')arr.push(string[i])
+   }
+   return arr.join('')
+    }
