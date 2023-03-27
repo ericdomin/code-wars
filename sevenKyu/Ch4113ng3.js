7f3330e83f6bd3ab33eba869c0c56c73ae26a450
Eric Domin <ericmdomin@gmail.com>
2023-03-27 14:00:49 -0400

    new codewars challenge

---
+++ sevenKyu/Ch4113ng3.js
@@ -0,0 +1,7 @@
+/*Make your strings more nerdy: Replace all 'a'/'A' with 4, 'e'/'E' with 3 and 'l' with 1 e.g. 
+"Fundamentals" --> "Fund4m3nt41s"
+
+*/
+function nerdify(txt){
+return txt.replace(/[aA]/g,'4').replace(/[l]/g,'1').replace(/[eE]/g,'3')
+}
